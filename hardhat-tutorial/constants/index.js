9b15e11d25a0fd694c27b4ce2af72ae13295043c
Thanh <thanh.nguyen@holdstation.com>
2022-08-26 12:00:48 +0700

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xeEd29417d75dA0635f0537fF816b8c747aA58a82";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
